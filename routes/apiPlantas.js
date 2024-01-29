659381523f91b65e6fbfd779a816d96e17498e30
Hegly <clasesccnn1@gmail.com>
2024-01-28 22:03:01 -0600

    controllers

--- routes/apiPlantas.js
+++ routes/apiPlantas.js
@@ -1,73 +1,17 @@
 import express from 'express'
 const planta = express();
-import {db} from '../db/conn.js'
+import {getPlanta,
+        postPlanta,
+        putPlanta,
+        deletePlanta
+    } from "../controllers/plantaController.js";
 
-let plantas = [{
-    nombre : "manzanilla",
-    tipo : "medicinal"
-},
-{
-    nombre : "ortencia",
-    tipo : "ornamental"
-}
-]
+planta.get('', getPlanta );
 
-planta.get('', async (req,res)=>{
+planta.post('', postPlanta);
 
-   const sql = `select * from tbl_planta order by id`
-   const result = await db.query(sql);
+planta.put( '/:id', putPlanta)
 
-   res.json(result)
-
-})
-
-planta.post('', async (req,res)=>{
-
-    const { nombre, tipo } = req.body;
-
-    const params = [nombre, tipo];
-
-    const sql = `insert into tbl_planta
-                (nombre, tipo)
-                values
-                ($1, $2) returning * `
-
-    const result = await db.query(sql, params);
-
-    res.json(result);
-})
-
-planta.put( '/:id', async (req, res)=>{
-
-    const { nombre, tipo } = req.body
-    const {id} = req.params
-
-    const params = [
-        nombre,
-        tipo,
-        id
-    ]
-
-    const sql = `update tbl_planta
-                set
-                    nombre = $1,
-                    tipo = $2
-                where id = $3 returning *`
-
-    const result = await db.query(sql, params)
-
-    res.json(result);
-} )
-
-planta.delete( '/:id', async (req, res)=>{
-
-    const params = [req.params.id];   ]
-
-    const sql = `delete from tbl_planta where id + $1 returning *`;
-
-    const result = await db.query(sql, params)
-
-    res.json(result);
-} )
+planta.delete( '/:id', deletePlanta )
 
 export { planta }
